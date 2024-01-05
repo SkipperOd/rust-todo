ccc0e88c66f9901503758050e28a9b0c80f6064a
kh_fawad <fawad@rentio.be>
2024-01-05 18:10:56 +0500

    feat(rocket): add json rocket feature
    add json feature and enabled it in the api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rocket::{
+    http::Status,
+    serde::json::{json, Value},
+};
+
 #[macro_use]
 extern crate rocket;
 
@@ -6,8 +11,30 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/")]
+fn hello() -> String {
+    String::from("form simple string")
+}
+
+#[get("/")]
+fn status() -> Status {
+    Status::NotExtended
+}
+
+#[get("/")]
+fn json_response() -> Value {
+    let var_name = json!({
+    "note": "it works"
+    });
+    var_name
+}
+
 #[launch]
 fn rocket() -> _ {
     println!("starting rocket server!!, by default port is 8000",);
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/hello", routes![hello])
+        .mount("/status", routes![status])
+        .mount("/json", routes![json_response])
 }
