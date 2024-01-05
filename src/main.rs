bc7fd241858c5e24be3df44b50fd5096c535aa3e
kh_fawad <fawad@rentio.be>
2024-01-05 16:54:42 +0500

    feat(rocket): install rocket framwork

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("git has been setup locally");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    println!("starting rocket server!!, by default port is 8000",);
+    rocket::build().mount("/", routes![index])
 }
